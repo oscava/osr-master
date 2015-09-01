23e7af517aaa75e70939639f4956515bb0c7c568
张嘉华 <admin@cavacn.com>
2015-09-01 20:08:34 +0800

    去除不必要部分
    去除不必要部分

--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@ var oOcp = new ocp("demo");
 
 var fs = require("fs");
 
-oOcp.start(fs.readFileSync(__dirname+"/code.js").toString(),[__dirname+"/code.js"],__dirname+"/code.js",{ channel: 123 , port: 112240});
+oOcp.start(__dirname+"/code.js",{ channel: 123 , port: 112240});
 
 oOcp.on("event",function(type, msg){
 	console.log("-->",type,msg);
